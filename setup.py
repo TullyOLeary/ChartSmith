56158bca49cc6d6028dc3bddae55effcebec9f0a
Tully OLeary <tullyro@gmail.com>
2024-08-05 16:49:04 -0500

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ChartSmith',
-    version='0.1.0',
+    version='0.1.1',
     author='Tully OLeary',
     author_email='tullyro@gmail.com',
     description='A package for creating well-formatted, well-labeled visualizations',
